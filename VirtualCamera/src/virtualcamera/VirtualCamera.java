f9d41aeee005904c6d47bcbc465a0991c4d713a2
Maciej <Maciej@192.168.0.10>
2018-10-28 13:06:26 +0100

    First commit!

---
+++ VirtualCamera/src/virtualcamera/VirtualCamera.java
@@ -0,0 +1,16 @@
+package virtualcamera;
+
+/**
+ *
+ * @author Maciej
+ */
+public class VirtualCamera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
